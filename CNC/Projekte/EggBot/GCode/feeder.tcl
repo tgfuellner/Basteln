38f7d08ee9ad708b85ecda52ac63ef7d7cf7987c
Thomas Gfüllner <thomas.gfuellner@gmx.de>
2011-07-04 18:23:37 +0200

    Baudrate einstellen

--- CNC/Projekte/EggBot/GCode/feeder.tcl
+++ CNC/Projekte/EggBot/GCode/feeder.tcl
@@ -2,11 +2,18 @@
 ## \
 exec tclsh "$0" ${1+"$@"}
 
+
+set BAUDRATE 57600
+set DEVICE /dev/ttyUSB1
+
+
+
 set fileToFeed [lindex $argv 0]
 
 
 set fdIn [open $fileToFeed]
-set fdOut [open /dev/ttyUSB1 r+]
+set fdOut [open $DEVICE r+]
+fconfigure $fdOut -mode $BAUDRATE,n,8,1
 
 
 while {[gets $fdIn line] >= 0} {
